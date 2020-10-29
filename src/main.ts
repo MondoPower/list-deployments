f83377d26e7a542f858ab98a00489dde957c294d
damian.bisignano <damian.bisignano@mondo.com.au>
2020-10-29 20:26:48 +1100

    asddasasd

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+const github = require('@actions/github');
+const core = require('@actions/core');
+
 
 type DeploymentState =
   | "error"
@@ -16,20 +17,38 @@ async function run() {
     const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
     const token = core.getInput("token", { required: true });
-    const environment =
-      core.getInput("environment", { required: false }) || "production";
+    const environment = core.getInput("environment", { required: false }) || "production";
 
     const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
 
-    const deployment = await client.repos.listDeployments({
+    const query = `
+    query($owner: String!, $repo: String!, $environment: String!) {
+      repository(owner:$owner, name:$repo) {
+        deployments(environments:$environment, first: 50, orderBy: {direction: DESC, field: CREATED_AT}) {
+          edges {
+            node {
+              createdAt
+              commit {
+                oid
+              }
+              state
+            }
+          }
+        }
+      }
+    }`;
+
+    const variables = {
       owner: context.repo.owner,
       repo: context.repo.repo,
-      environment: environment,
-    })
-    
-    deployment.data.forEach(a => console.log(a.sha));
+      environment: environment
+    };
 
-    core.setOutput("sha", deployment.data[0].sha);
+    const deployment = await client.graphql(query, variables);
+
+    console.log(deployment);
+
+    core.setOutput("sha", "as");
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
