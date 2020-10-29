e15fdec91ae34f04fc8104b1ed2d1702c4afb9e9
damian.bisignano <damian.bisignano@mondo.com.au>
2020-10-29 15:25:00 +1100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+type DeploymentState =
+  | "error"
+  | "failure"
+  | "inactive"
+  | "in_progress"
+  | "queued"
+  | "pending"
+  | "success";
+
+async function run() {
+  try {
+    const context = github.context;
+    const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+
+    const token = core.getInput("token", { required: true });
+    const environment =
+      core.getInput("environment", { required: false }) || "production";
+
+    const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
+
+    const deployment = await client.repos.listDeployments({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      environment: environment,
+    })
+
+    core.setOutput("sha", deployment.data[0].sha);
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
